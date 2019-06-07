154a958105dcc9b73f2078c53e5d456f387446a4
Zoeckler <lzoeckler@path.org>
2019-06-07 13:32:44 -0700

    updated python script to match notebook

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -2,25 +2,25 @@ import pandas as pd
 import numpy as np
 from functools import partial, reduce
 # import helper function
-from data_processing.data_processing_helpers import run_compare
+from data_processing.data_processing_helpers import run_compare, return_decisions
 # import constants
-from data_processing.data_processing_helpers import (DECISIONS,
-                                                     THRESHOLDS)
+from data_processing.data_processing_helpers import (DIL_CONSTANTS, DILUTION_SETS, THRESHOLDS)
 
 
-for fname in ["file", "paths", "here"]:
-    # read in data from flat file, columns must be in correct order
-    plex_data = pd.read_csv(fname, skiprows=8, names=['patient_id', 'type',
-                                                      'well', 'error',
-                                                      'HRP2_pg_ml',
-                                                      'LDH_Pan_pg_ml',
-                                                      'LDH_Pv_pg_ml',
-                                                      'CRP_ng_ml'])
-    plex_data = plex_data.applymap(lambda x: x.lower() if isinstance(
-        x, str) else x)
-    plex_data['patient_id'] = plex_data['patient_id'].fillna(method='ffill')
+def main():
+    dfs = []
+    for fname in ["file", "paths", "here"]:
+        # read in data from flat file, columns must be in correct order
+        plex_data = pd.read_csv('C:/Users/lzoeckler/Desktop/4plex/test_data/{}.csv'.format(fname),
+                                skiprows=8, names=['patient_id', 'type', 'well', 'error',
+                                                   'HRP2_pg_ml', 'LDH_Pan_pg_ml',
+                                                   'LDH_Pv_pg_ml', 'CRP_ng_ml'])
+        plex_data = plex_data.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+        plex_data['patient_id'] = plex_data['patient_id'].fillna(method='ffill')
+        dfs.append(plex_data)
+    combined = pd.concat(dfs)
     # subset data to just what we want
-    samples_data = plex_data.loc[plex_data['patient_id'].str.contains('pa-')]
+    samples_data = combined.loc[combined['patient_id'].str.contains('pa-')]
     samples_data = samples_data.drop('type', axis=1)
     # rename patient_id and create concentration column
     samples_data['concentration'] = samples_data['patient_id'].apply(
@@ -28,57 +28,68 @@ for fname in ["file", "paths", "here"]:
     samples_data['patient_id'] = samples_data['patient_id'].apply(
         lambda x: x.partition(' ')[0])
     # subset data a final time
-    samples_data = samples_data.loc[
-        (samples_data['concentration'] == '(neat)') |
-        (samples_data['concentration'].str.contains('50x'))]
-    samples_data = samples_data.loc[~samples_data[
-        'concentration'].str.contains('low volume')]
+    subset_str = 'neat|50x|2500x|125000x|6250000x|312500000x'
+    samples_data = samples_data.loc[(samples_data['concentration'].str.contains(subset_str))]
+    samples_data = samples_data.loc[~samples_data['concentration'].str.contains('low volume')]
+    samples_data = samples_data.loc[~samples_data['well'].isnull()]
+    samples_data = samples_data.sort_values(['patient_id', 'well'])
 
     # generate an empty list to fill with small dfs, which will be combined
     final_dfs = []
     # run counts for decision on what to keep
     for analyte in THRESHOLDS.keys():
-        # create partial function for generating decision vectors
-        partial_compare = partial(run_compare, analyte_val=analyte)
-        # generate decision vectors
-        samples_data['decision_vector'] = samples_data.apply(partial_compare, axis=1)
-        # pull decision matrix for given analyte
-        decision_matrix = DECISIONS[analyte]
-        # generate an empty list to fill with tiny dfs, which will be combined
-        tiny_dfs = []
-        # iterate over patient_ids
-        for i in samples_data['patient_id'].unique().tolist():
-            tiny_df = pd.DataFrame(columns=['patient_id', analyte,
-                                            '{}_dilution'.format(analyte),
-                                            '{}_well'.format(analyte)])
-            sub_data = samples_data.loc[samples_data['patient_id'] == i]
-            neat_vector = sub_data.loc[sub_data['concentration'] == '(neat)',
-                                       'decision_vector'].item()
-            dil_vector = sub_data.loc[sub_data['concentration'].str.contains('50x'),
-                                      'decision_vector'].item()
-            decision = decision_matrix[neat_vector, dil_vector].item()
-            pos_val = 'negative'
-            if decision == '1:50':
-                val = sub_data.loc[sub_data['concentration'].str.contains('50x'),
-                                   analyte].item()
-                well = sub_data.loc[sub_data['concentration'].str.contains('50x'),
-                                    'well'].item()
-            elif decision == 'neat':
-                val = sub_data.loc[sub_data['concentration'] == '(neat)',
-                                   analyte].item()
-                well = sub_data.loc[sub_data['concentration'] == '(neat)',
-                                    'well'].item()
-            elif decision == 'fail':
-                val = np.nan
-                well = np.nan
-            else:
-                raise ValueError("Unexpected decision value: {}".format(decision))
-            tiny_df = tiny_df.append({'patient_id': i, analyte: val,
-                                      '{}_dilution'.format(analyte): decision,
-                                      '{}_well'.format(analyte): well},
-                                     ignore_index=True)
-            tiny_dfs.append(tiny_df)
-        small_df = pd.concat(tiny_dfs)
-        final_dfs.append(small_df)
-    output_df = reduce(lambda left, right: pd.merge(left, right, on='patient_id'),
+        dil_dfs = []
+        for max_dilution in DIL_CONSTANTS.keys():
+            # create partial function for generating decision vectors
+            partial_compare = partial(run_compare, analyte_val=analyte)
+            # generate decision vectors
+            samples_data['decision_vector'] = samples_data.apply(partial_compare, axis=1)
+            # generate all decicion matrices given current concentrations
+            low, high, fail = DILUTION_SETS[max_dilution]
+            decisions = return_decisions(low, high, fail)
+            # pull decision matrix for given analyte
+            decision_matrix = decisions[analyte]
+            # generate an empty list to fill with tiny dfs, which will be combined
+            tiny_dfs = []
+            # iterate over patient_ids
+            for i in samples_data['patient_id'].unique().tolist():
+                tiny_df = pd.DataFrame(columns=['patient_id', analyte,
+                                                '{}_dilution'.format(analyte),
+                                                '{}_well'.format(analyte)])
+                tiny_df['comparison'] = '{} vs {}'.format(low, high)
+                tiny_df = tiny_df[['patient_id', 'comparison', '{}_dilution'.format(analyte),
+                                   '{}_well'.format(analyte)]]
+                sub_data = samples_data.loc[samples_data['patient_id'] == i]
+                if len(sub_data) == (len(DIL_CONSTANTS) + 1):
+                    vector_low = sub_data.loc[sub_data['concentration'].str.contains(low),
+                                              'decision_vector'].item()
+                    vector_high = sub_data.loc[sub_data['concentration'].str.contains(high),
+                                               'decision_vector'].item()
+                    decision = decision_matrix[vector_low, vector_high].item()
+                    if decision in [low, high]:
+                        val = sub_data.loc[sub_data['concentration'].str.contains(decision),
+                                           analyte].item()
+                        well = sub_data.loc[sub_data['concentration'].str.contains(decision),
+                                            'well'].item()
+                    elif decision == fail:
+                        val = np.nan
+                        well = np.nan
+                    else:
+                        raise ValueError("Unexpected decision value: {}".format(decision))
+                    tiny_df = tiny_df.append({'patient_id': i, 'comparison': '{} vs {}'.format(low, high),
+                                              analyte: val, '{}_dilution'.format(analyte): decision,
+                                              '{}_well'.format(analyte): well}, ignore_index=True)
+                    tiny_dfs.append(tiny_df)
+                else:
+                    continue
+            tiny_df = pd.concat(tiny_dfs)
+            dil_dfs.append(tiny_df)
+        dil_df = pd.concat(dil_dfs)
+        final_dfs.append(dil_df)
+    output_df = reduce(lambda left, right: pd.merge(left, right, on=['comparison', 'patient_id']),
                        final_dfs)
+    return output_df
+
+
+if __name__ == '__main__':
+    main()
--- data_processing/data_processing_helpers.py
+++ data_processing/data_processing_helpers.py
@@ -4,54 +4,48 @@ import numpy as np
 
 # function for creating decision vector based on antigen value
 # at a specific concentration
-def run_compare(df, analyte_val):
-    above, below, llq, ulq, na = False, False, False, False, False
-    value = df[analyte_val]
+def run_compare(df, analyte_val, dil_val):
+    above, below, llw, ulq, na = False, False, False, False, False
+    val = df[analyte_val]
+    thresh_val = DIL_CONSTANTS[dil_val] * THRESHOLDS[analyte_val]
     try:
-        float_val = float(value)
+        float_val = float(val)
         if math.isnan(float_val):
             na = True
-        elif float_val > THRESHOLDS[analyte_val]:
+        elif float_val > thresh_val:
             above = True
-        elif float_val < THRESHOLDS[analyte_val]:
+        elif float_val < thresh_val:
             below = True
     except ValueError:
-        if '<' in value:
+        if '<' in val:
             llq = True
-        elif '>' in value:
+        elif '>' in val:
             ulq = True
     finally:
         return np.array([above, below, llq, ulq, na])
 
 
 # create decision matrices for determining which concentration to use
-HRP2_MATRIX = np.array([['1:50', '1:50', '1:50', '1:50', '1:50'],
-                        ['1:50', 'neat', 'neat', '1:50', 'fail'],
-                        ['1:50', 'neat', 'neat', 'fail', 'fail'],
-                        ['1:50', '1:50', 'fail', '1:50', '1:50'],
-                        ['fail', '1:50', '1:50', 'fail', 'fail']])
+def return_decisions(low, high, fail):
+    # Columns = [neat_above, neat_below, neat_LLQ, neat_ULQ, NA]
+    # Rows = [dil_above, dil_below, dil_LLQ, dil_ULQ, NA]
+    hrp2_matrix = np.array([[high, high, high, high, high],
+                            [high, low, low, high, fail],
+                            [high, low, low, fail, fail],
+                            [high, high, fail, high, high],
+                            [fail, high, high, fail, fail]])
 
-LDH_PAN_MATRIX = np.array([['1:50', 'neat', 'neat', '1:50', '1:50'],
-                           ['1:50', 'neat', 'neat', '1:50', 'fail'],
-                           ['1:50', 'neat', 'neat', 'fail', 'fail'],
-                           ['1:50', 'neat', 'fail', '1:50', '1:50'],
-                           ['fail', 'neat', 'neat', 'fail', 'fail']])
+    other_matrix = np.array([[high, low, low, high, high],
+                               [high, low, low, high, fail],
+                               [high, low, low, fail, fail],
+                               [high, low, fail, high, high],
+                               [fail, low, low, fail, fail]])
 
-LDH_PV_MATRIX = np.array([['1:50', 'neat', 'neat', '1:50', '1:50'],
-                          ['1:50', 'neat', 'neat', '1:50', 'fail'],
-                          ['1:50', 'neat', 'neat', 'fail', 'fail'],
-                          ['1:50', 'neat', 'fail', '1:50', '1:50'],
-                          ['fail', 'neat', 'neat', 'fail', 'fail']])
+    # decisions for various analytes
+    decisions = {'HRP2_pg_ml': hrp2_matrix, 'LDH_Pan_pg_ml': other_matrix,
+                 'LDH_Pv_pg_ml': other_matrix, 'CRP_ng_ml': other_matrix}
+    return decisions
 
-CRP_MATRIX = np.array([['1:50', 'neat', 'neat', '1:50', '1:50'],
-                       ['1:50', 'neat', 'neat', '1:50', 'fail'],
-                       ['1:50', 'neat', 'neat', 'fail', 'fail'],
-                       ['1:50', 'neat', 'fail', '1:50', '1:50'],
-                       ['fail', 'neat', 'neat', 'fail', 'fail']])
-
-# decisions for various analytes
-DECISIONS = {'HRP2_pg_ml': HRP2_MATRIX, 'LDH_Pan_pg_ml': LDH_PAN_MATRIX,
-             'LDH_Pv_pg_ml': LDH_PV_MATRIX, 'CRP_ng_ml': CRP_MATRIX}
 
 # threshhold values for various analytes
 THRESHOLDS = {'HRP2_pg_ml': 330, 'LDH_Pan_pg_ml': 10514,
@@ -60,3 +54,13 @@ THRESHOLDS = {'HRP2_pg_ml': 330, 'LDH_Pan_pg_ml': 10514,
 # positivity threshholds for various analytes
 POS_THRESHOLDS = {'HRP2_pg_ml': 2.3, 'LDH_Pan_pg_ml': 47.8,
                   'LDH_Pv_pg_ml': 75.1, 'CRP_ng_ml': np.nan}
+
+# constant to apply to the threshhold for different dilutions
+DIL_CONSTANTS = {'50x': 1, '2500x': 50, '125000x': 2500,
+                 '6250000x': 125000, '312500000x': 6250000}
+
+# dilution sets for various dilutions
+DILUTION_SETS = {'50x': ('neat', '50x', 'fail'), '2500x': ('50x', '2500x', 'fail'),
+                 '125000x': ('2500x', '125000x', 'fail'),
+                 '6250000x': ('125000x', '6250000x', 'fail'),
+                 '312500000x': ('6250000x', '312500000x', 'fail')}
