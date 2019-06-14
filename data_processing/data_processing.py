9e115af757f4c86a223fd4de3c7f8e7b54418fb0
Zoeckler <lzoeckler@path.org>
2019-06-14 14:10:13 -0700

    big ol update

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -39,16 +39,61 @@ def main():
     samples_data['concentration'] = samples_data.apply(fix_concentrations, axis=1)
     samples_data = samples_data.sort_values(['patient_id', 'well'])
 
+    # subset the data to just duplicates
+    duplicates = samples_data.loc[samples_data.duplicated(subset=['patient_id', 'concentration'], keep=False)]
+    deduped_dfs = []
+    for analyte in THRESHOLDS.keys():
+        dup_analyte = duplicates[['patient_id', 'well', 'error', 'concentration', analyte]]
+        pid_dfs = []
+        for pid in duplicates['patient_id'].unique():
+            dup_data = dup_analyte.loc[dup_analyte['patient_id'] == pid]
+            con_dfs = []
+            for concentration in dup_data['concentration'].unique():
+                fill_df = pd.DataFrame(columns=['patient_id', 'well', 'error',
+                                                'concentration', analyte])
+                # everything until this is subsetting to specific data
+                dup_con = dup_data.loc[dup_data['concentration'] == concentration]
+                # get the values for the duplicate concentrations
+                values = dup_con[analyte]
+                # also preserve wells and errors for duplicate concentrations
+                wells = dup_con['well'].tolist()
+                wells = ''.join(c for c in str(wells) if c not in ["[", "]", "'"])
+                errors = dup_con['error'].tolist()
+                errors = ''.join(c for c in str(errors) if c not in ["[", "]", "'"])
+                try:
+                    # if they're both real numbers, take the average
+                    values = [float(val) for val in values.tolist()]
+                    val = sum(values) / len(values)
+                except ValueError:
+                    values = values.tolist()
+                    num_vals = [val for val in values if ('<' not in val) & ('>' not in val)]
+                    # if one is a real number, take that one
+                    if len(num_vals) == 1:
+                        val = num_vals[0]
+                    # if both are non-real, we assume they're the same. maybe sketchy?
+                    else:
+                        val = values[0]
+                fill_df = fill_df.append({'patient_id': pid, 'well': wells, 'error': errors,
+                                          'concentration': concentration, analyte: val}, ignore_index=True)
+                con_dfs.append(fill_df)
+            con_df = pd.concat(con_dfs)
+            pid_dfs.append(con_df)
+        pid_df = pd.concat(pid_dfs)
+        deduped_dfs.append(pid_df)
+    deduped = reduce(lambda left, right: pd.merge(left, right, on=['patient_id', 'well', 'error', 'concentration']),
+                     deduped_dfs)
+    # replace old duplicated values with new dedeuplicated values
+    no_duplicates = samples_data.drop_duplicates(subset=['patient_id', 'concentration'], keep=False)
+    no_duplicates = pd.concat([no_duplicates, deduped])
+
     # create an empty list to fill with small dfs, which will be combined
     analyte_dfs = []
-    # create an empty list to fill with problem patient_ids
-    error_ids = []
     # run counts for decision on what to keep
     for analyte in THRESHOLDS.keys():
         patient_dfs = []
         # iterate over patient_ids
         for i in samples_data['patient_id'].unique():
-            patient_data = samples_data.loc[samples_data['patient_id'] == i]
+            patient_data = no_duplicates.loc[no_duplicates['patient_id'] == i]
             # get number of dilutions
             dilution_values = sorted([val for val in patient_data['concentration'].unique() if val != '1'], key=len)
             # set initial best decision to neat (1)
@@ -68,34 +113,27 @@ def main():
                 best_df = pd.DataFrame(columns=['patient_id', analyte,
                                                 '{}_dilution'.format(analyte),
                                                 '{}_well'.format(analyte)])
-                try:
-
-                    vector_low = dil_data.loc[dil_data['concentration'] == best_decision,
-                                              'decision_vector'].item()
-                    vector_high = dil_data.loc[dil_data['concentration'] == max_dilution,
-                                               'decision_vector'].item()
-                    decision = decision_matrix[vector_high, vector_low].item()
-                    if decision in [best_decision, max_dilution]:
-                        val = dil_data.loc[dil_data['concentration'] == decision,
-                                           analyte].item()
-                        well = dil_data.loc[dil_data['concentration'] == decision,
-                                            'well'].item()
-                        best_decision = decision
-                    elif decision == 'fail':
-                        val = 'fail'
-                        well = 'fail'
-                    else:
-                        raise ValueError("Unexpected decision value: {}".format(decision))
-                    best_df = best_df.append({'patient_id': i, analyte: val,
-                                              '{}_dilution'.format(analyte): decision,
-                                              '{}_well'.format(analyte): well}, ignore_index=True)
-                    best_decision = decision
-                    if decision == 'fail':
-                        break
-                except ValueError as e:
-                    if str(e) != 'can only convert an array of size 1 to a Python scalar':
-                        raise
-                    error_ids.append(i)
+                vector_low = dil_data.loc[dil_data['concentration'] == best_decision,
+                                          'decision_vector'].item()
+                vector_high = dil_data.loc[dil_data['concentration'] == max_dilution,
+                                           'decision_vector'].item()
+                decision = decision_matrix[vector_high, vector_low].item()
+                if decision in [best_decision, max_dilution]:
+                    val = dil_data.loc[dil_data['concentration'] == decision,
+                                       analyte].item()
+                    well = dil_data.loc[dil_data['concentration'] == decision,
+                                        'well'].item()
+                elif decision == 'fail':
+                    val = 'fail'
+                    well = 'fail'
+                else:
+                    raise ValueError("Unexpected decision value: {}".format(decision))
+                best_df = best_df.append({'patient_id': i, analyte: val,
+                                          '{}_dilution'.format(analyte): decision,
+                                          '{}_well'.format(analyte): well}, ignore_index=True)
+                best_decision = decision
+                if decision == 'fail':
+                    break
             patient_dfs.append(best_df)
         patient_df = pd.concat(patient_dfs)
         analyte_dfs.append(patient_df)
