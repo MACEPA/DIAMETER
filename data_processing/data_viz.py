655e9eadc39d5821926c89b022b9979292d5bab8
Zoeckler <lzoeckler@path.org>
2019-07-23 15:53:30 -0700

    fixing % point

--- data_processing/data_viz.py
+++ data_processing/data_viz.py
@@ -217,7 +217,7 @@ def hrp2_grouping(main_data, analyte, analyte_name):
             # and
             # 3) If the difference between the value and the mean of the values at the first three time points is
             # greater than 1% of that mean and
-            # 4) If the immediately following value is greater than 90% of the mean of the values at the first three
+            # 4) If the immediately following value is greater than 80% of the mean of the values at the first three
             # time points, then
             # 5) The patient ID is set to group red
             for i in range(len(sub_val)):
