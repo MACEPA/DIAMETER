3430ce42fae856d0ff72ebab2bb83d437ff9fcda
Zoeckler <lzoeckler@path.org>
2020-01-27 12:53:07 -0800

    starting to comment code

--- data_processing/PCR/threshold_graph.py
+++ data_processing/PCR/threshold_graph.py
@@ -10,54 +10,85 @@ from threshold_helpers import clean_strings
 
 
 def main(input_dir, output_dir, lower_threshold, upper_threshold, run_single, run_dual, show):
+    # Convert pg values to ng for labeling
     ng_lower = lower_threshold / 1000
     ng_upper = upper_threshold / 1000
 
+    # Read in all CSVs
     all_files = glob.glob('{}/*.csv'.format(input_dir))
     big_df = []
     for file in all_files:
         df = pd.read_csv(file)
         big_df.append(df)
     big_df = pd.concat(big_df, sort=True)
+    # Subset to CSVs where PCR results are non-null
     big_df = big_df.loc[~big_df['PCR_pos'].isnull()]
 
+    # List out columns that need to be cleaned
     val_cols = ['quansys_HRP2_pg_ml', 'quansys_LDH_Pan_pg_ml', 'quansys_LDH_Pv_pg_ml',
                 'quansys_LDH_Pf_pg_ml', 'quansys_CRP_ng_ml']
 
+    # Subset dataframe to just positive PCR cases
     pos_df = big_df.loc[big_df['PCR_pos'] == 1]
+    # Drop rows with null LDH or HRP2
     pos_df = pos_df.loc[~pos_df['quansys_LDH_Pv_pg_ml'].isnull()]
     pos_df = pos_df.loc[~pos_df['quansys_HRP2_pg_ml'].isnull()]
+    # Clean columns listed above
     pos_df[val_cols] = pos_df[val_cols].applymap(clean_strings)
 
+    # Copy positive dataframe to manipulate
     den_df = pos_df.copy(deep=True)
+    # Subset copy to only cleaned columns, PCR status, and sample_id
     den_df = den_df[val_cols + ['PCR_pos', 'sample_id']]
 
+    # Copy positive dataframe to manipulate
     feb_df = pos_df.copy(deep=True)
+    # Subset copy to febrile cases
     feb_df = feb_df.loc[feb_df['febrile'] == 1]
+    # Create a new dataframe with only Pf+ cases
     feb_pf_df = feb_df.loc[feb_df['pf'] == 1]
-    feb_pv_df = feb_df.loc[feb_df['pv'] == 1]
+    # Subset to only required columns
     feb_pf_df = feb_pf_df[val_cols + ['PCR_pos', 'sample_id']]
+    # Create a new dataframe with only Pv+ cases
+    feb_pv_df = feb_df.loc[feb_df['pv'] == 1]
+    # Subset to only required columns
     feb_pv_df = feb_pv_df[val_cols + ['PCR_pos', 'sample_id']]
+    # Subset entire febrile case dataframe to only required columns
     feb_df = feb_df[val_cols + ['PCR_pos', 'sample_id']]
 
+    # Copy positive dataframe to manipulate
     non_feb_df = pos_df.copy(deep=True)
+    # Subset copy to non-febrile cases
     non_feb_df = non_feb_df.loc[non_feb_df['febrile'] == 0]
+    # Create a new dataframe with only Pf+ cases
     non_feb_pf_df = non_feb_df.loc[non_feb_df['pf'] == 1]
-    non_feb_pv_df = non_feb_df.loc[non_feb_df['pv'] == 1]
+    # Subset to only required columns
     non_feb_pf_df = non_feb_pf_df[val_cols + ['PCR_pos', 'sample_id']]
+    # Create a new dataframe with only Pv+ cases
+    non_feb_pv_df = non_feb_df.loc[non_feb_df['pv'] == 1]
+    # Subset to only required columns
     non_feb_pv_df = non_feb_pv_df[val_cols + ['PCR_pos', 'sample_id']]
+    # Subset entire non-febrile case dataframe to only required columns
     non_feb_df = non_feb_df[val_cols + ['PCR_pos', 'sample_id']]
 
+    # Copy positive dataframe to manipulate
     pf_df = pos_df.copy(deep=True)
+    # Subset copy to Pf+ cases
     pf_df = pf_df.loc[pf_df['pf'] == 1]
+    # Subset to only required columns
     pf_df = pf_df[val_cols + ['PCR_pos', 'sample_id']]
 
+    # Copy positive dataframe to manipulate
     pv_df = pos_df.copy(deep=True)
+    # Subset copy to Pv+ cases
     pv_df = pv_df.loc[pv_df['pv'] == 1]
+    # Subset to only required columns
     pv_df = pv_df[val_cols + ['PCR_pos', 'sample_id']]
 
+    # Run the following code only if single graphs (with one density line) are desired
     if run_single:
         print('Running single plots')
+        # Collect all dataframes generated above and associate them with a little metadata
         value_list = [(den_df, 'PCR+ pLDH', 'density_plot'), (feb_df, 'Febrile PCR+ pLDH', 'febrile_plot'),
                       (feb_pf_df, 'Febrile Pf+ PCR+ pLDH', 'febrile_pf_plot'),
                       (feb_pv_df, 'Febrile Pv+ PCR+ pLDH', 'febrile_pv_plot'),
@@ -66,7 +97,9 @@ def main(input_dir, output_dir, lower_threshold, upper_threshold, run_single, ru
                       (non_feb_pv_df, 'Non-febrile Pv+ PCR+ pLDH', 'non_febrile_pv_plot'),
                       (pf_df, 'Pf+ PCR+ pLDH', 'pf_plot'), (pv_df, 'Pv+ PCR+ pLDH', 'pv_plot')]
 
+        # Run over collected dataframes and metadata
         for test_df, pdf_title, plot_title in value_list:
+            # Get density distribution of pLDH values
             x = test_df['quansys_LDH_Pan_pg_ml'].values
 
             sub = test_df.loc[test_df['quansys_LDH_Pan_pg_ml'] > np.log10(lower_threshold)]
@@ -111,6 +144,7 @@ def main(input_dir, output_dir, lower_threshold, upper_threshold, run_single, ru
             plt.close()
             pp.close()
 
+    # Run the following code only if dual graphs (with two density lines, febrile and non) are desired
     if run_dual:
         print('Running dual plots')
         for feb_df, non_feb_df, species in [(feb_pf_df, non_feb_pf_df, 'Pf'), (feb_pv_df, non_feb_pv_df, 'Pv')]:
